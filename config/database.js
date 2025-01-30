3248c5cce07ed9cd5f6dfdb85b1964113932bd24
James Jahner <jitone1@gmail.com>
2025-01-29 21:14:37 -0600

    moved the connectDB to database.js

--- config/database.js
+++ config/database.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 
+//const renderDB =require('../etc/secrets/DB_STRING.env')//'../etc/secrets')
+
 const connectDB = async () => {
     try {
         console.log('Connecting to MongoDB...');
         const conn = await mongoose.connect(process.env.DB_STRING);
+        //const conn = await mongoose.connect(renderDB)//process.env.DB_STRING);
+
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (err) {
         console.error('Database Connection Error:', err.message);
